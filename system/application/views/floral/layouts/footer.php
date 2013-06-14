1390f668f34de19de4e25623d6d80d8298bf2b14
Purwa@Purwa-PC <Purwa@Purwa-PC>
2013-06-14 08:37:24 +0700

    update footer, script check isset dulu

--- system/application/views/floral/layouts/footer.php
+++ system/application/views/floral/layouts/footer.php
@@ -6,7 +6,7 @@
 <script type="text/javascript">
     <!--//
     $(document).ready(function(){
-       <?php echo $script ?>
+       <?php echo isset($script)?$script:'' ?>
     });
     //-->
 </script>
