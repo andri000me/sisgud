6610e1037a7f9437adbcc49cf9eebbdedf91f581
Purwa@Purwa-PC <Purwa@Purwa-PC>
2013-12-28 10:01:55 +0700

    update row total = total harga sebenarnya, bukan dtambah 15%

--- system/application/controllers/gudang.php
+++ system/application/controllers/gudang.php
@@ -477,7 +477,7 @@ class Gudang extends Controller {
                                 $hp = $row->item_hp;
                                 $this->data['sup_region'] = 'MDN';
                             }
-                            $total += $hp*$row->qty;
+                            $total += $row->item_hp*$row->qty;
                             $total_qty += $row->qty;
                             
                             $data .= '<td style="width: 50px;text-align: right;">'.number_format($hp,'0',',','.').',-</td>
@@ -581,7 +581,7 @@ class Gudang extends Controller {
                                 $hp = $row->item_hp;
                                 $this->data['sup_region']='MDN';
                             }
-                            $total += ($hp*$row->qty);
+                            $total += ($row->item_hp*$row->qty);
                             $data .= '	<td style="width: 50px;text-align: right;">'.number_format($hp,'0',',','.').',-</td>
                                     <td style="width: 60px;text-align: center;"></td>		
                                     </tr>';
